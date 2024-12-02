3a00a28db30159911932426db4f6ae68992a470b
gabrielfelipeee <gabrielfeliperodrigues78@gmail.com>
2024-12-02 17:54:37 -0300

    Base do projeto

---
+++ src/Api.Application/Program.cs
@@ -0,0 +1,25 @@
+namespace Api.Application
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddControllers();
+
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseHttpsRedirection();
+
+            app.Run();
+        }
+    }
+}
